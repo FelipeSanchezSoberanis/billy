45bad06009e98a3c1048360a228750f7b426f4c2
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-04-19 01:56:04 -0600

    Step and reset

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,37 @@
-void setup() { pinMode(LED_BUILTIN, OUTPUT); }
+#include <Servo.h>
+
+#define ANGLE_STEP 45;
+
+Servo servo;
+
+int angle;
+String input;
+
+void setup() {
+    Serial.begin(9600);
+
+    servo.attach(9);
+
+    angle = 0;
+    input = "";
+}
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    if (Serial.available()) {
+        input = Serial.readString();
+        input.trim();
+        input.toLowerCase();
+    }
+
+    if (input == "spin") {
+        angle += ANGLE_STEP;
+        servo.write(angle);
+        input = "";
+    }
+
+    if (input == "reset") {
+        angle = 0;
+        servo.write(angle);
+        input = "";
+    }
 }
